f1d8eb83eaa748bcc3eca0cab0d0b72b73cdb6cf
Jason Callaway <jason@jasoncallaway.com>
2018-03-14 12:26:49 -0400

    added control filter for oscap-generated yaml

---
+++ files/gap/control_filter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import yaml
+import sys
+
+try:
+    with open(sys.argv[1], 'r') as f:
+        content = yaml.load(f)
+except Exception as e:
+    print('ERROR: failed to read yaml file: ' + str(e))
+    exit(1)
+
+for play in content:
+    for task in play['tasks']:
+        if task.get('tags'):
+            for tag in task['tags']:
+                if 'NIST-800-53' in tag:
+                    control = tag.replace('NIST-800-53-', '')
+                    print(control + ',' + task['name'])
+
+1
