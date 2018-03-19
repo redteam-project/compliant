35bf6d1963096c73e1781523cd9ded80231dc50f
Jason Callaway <jason@jasoncallaway.com>
2018-03-19 09:10:28 -0400

    updated control filter

--- files/gap/control_filter.py
+++ files/gap/control_filter.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python
 
-import yaml
+import os
 import sys
+import uuid
+import yaml
+
+
+def find_yaml(path):
+    yaml_files = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if '.yml' in f:
+                yaml_files.append(root + '/' + f)
+        for d in dirs:
+            if d != '.':
+                yaml_files.extend(find_yaml(d))
+    return yaml_files
 
 try:
-    with open(sys.argv[1], 'r') as f:
-        content = yaml.load(f)
+    plays = []
+    for d in sys.argv[1:]:
+        for yaml_file in find_yaml(d):
+            with open(yaml_file, 'r') as f:
+                play = yaml.load(f)
+                for i, p in enumerate(play):
+                    play[i]['filename'] = f.name
+                plays.extend(play)
 except Exception as e:
-    print('ERROR: failed to read yaml file: ' + str(e))
+    print('ERROR: ' + str(e))
     exit(1)
 
-for play in content:
-    for task in play['tasks']:
-        if task.get('tags'):
-            for tag in task['tags']:
-                if 'NIST-800-53' in tag:
-                    control = tag.replace('NIST-800-53-', '')
-                    print(control + ',' + task['name'])
+all_controls = []
+for play in plays:
+    if play.get('tasks'):
+        for task in play['tasks']:
+            if task.get('tags'):
+                for tag in task['tags']:
+                    if 'NIST-800-53' in tag:
+                        control = tag.replace('NIST-800-53-', '')
+                        all_controls.append(control + ',' + task['name'] + ',' + play['filename'])
+                    else:
+                        all_controls.append(tag + ',' + task['name'] + ',' + play['filename'])
+
+print('\n'.join(sorted(all_controls)))
